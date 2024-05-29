0686a915e5906b3f9b9897155a9628a275329943
Anastasia Mik <mikunova1108@gmail.com>
2024-05-29 19:54:27 +0300

    Add fun homepage

--- src/Controller/VynilController.php
+++ src/Controller/VynilController.php
@@ -1,12 +1,27 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
 class VynilController
 {
     #[Route('/')]
-    public function homepage()
+    public function homepage(): Response
     {
-        die('Vinyl: Definitely NOT a fancy-looking frisbee!');
+        return new Response('Title: PB and Jams');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true) ;
+        } else{
+            $title = 'All Genres';
+        }
+
+        return new Response($title);
     }
 }
